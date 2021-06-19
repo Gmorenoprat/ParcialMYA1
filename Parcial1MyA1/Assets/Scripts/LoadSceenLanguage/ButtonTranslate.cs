527af2d8a5c11b5a15bb6dcab0be6e0d7b294c34
mariano munguia <marianoa.munguia@gmail.com>
2021-06-19 15:25:29 -0300

    SceenLoad -- Done 1/2

---
+++ Parcial1MyA1/Assets/Scripts/LoadSceenLanguage/ButtonTranslate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTranslate : MonoBehaviour
+{
+    public string ID;
+
+    public LangManager manager;
+
+    public Text myView;
+
+    string _myText = "";
+
+    void Awake()
+    {
+        _myText = myView.GetComponent<Text>().text;
+
+        if (!manager)
+        {
+            manager = GameObject.Find("LanguageManager").GetComponent<LangManager>();
+        }
+            
+       manager.OnUpdate += ChangeLang;
+        
+    }
+    private void Start()
+    {
+        try { ChangeLang(); }
+        catch { return; }
+    }
+
+
+    void ChangeLang()
+    {
+        myView.text = manager.GetTranslate(ID);
+    }
+}
